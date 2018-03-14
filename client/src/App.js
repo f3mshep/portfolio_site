5cecab93039ef2e4a1d9890c6b6e175a06dadb3d
f3mshep <superbiscuit@gmail.com>
2018-03-14 16:31:23 -0700

    added react-router. Single pages apps wooo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PageContainer from './containers/page_container';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import BlogIndex from './containers/blog_container'
 
 class App extends Component {
   render() {
-    return (
-      <PageContainer/>
-    );
+    return <Router>
+        <Route path="/" component={PageContainer} />
+        <Route path="/blogs" component={BlogIndex} />
+      </Router>;
   }
 }
 
